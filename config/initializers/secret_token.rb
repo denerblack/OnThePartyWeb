a5457c057ca44d31665b5105b07179c96a1132e8
Dener Wilian Pereira do Carmo <dener.wilian@gmail.com>
2012-05-29 11:45:49 -0400

    Iniciando projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnTheParty::Application.config.secret_token = 'f958fe26c03dee9ef7a76135b8ea057695b56cf397b65aa15c867e608c90f64f12793301508c0a1075374a010b497425dcc41c373b51b5bf3ad15135e2979ac7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+OnTheParty::Application.config.session_store :cookie_store, key: '_on_the_party_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# OnTheParty::Application.config.session_store :active_record_store
