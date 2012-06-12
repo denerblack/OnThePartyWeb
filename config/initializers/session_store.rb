e8442fbf75508ecefa8baac86fd0aaefdc755bb4
Dener Wilian Pereira do Carmo <dener.wilian@gmail.com>
2012-06-12 00:34:23 -0400

    Adicionado execjs no bundle

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-OnTheParty::Application.config.session_store :cookie_store, key: '_on_the_party_session'
+OnTheParty::Application.config.session_store :cookie_store, :key => '_on_the_party_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
