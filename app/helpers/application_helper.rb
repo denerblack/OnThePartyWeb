506b141d8e204ee0475ea0244d1fe6369e50826e
Rodrigo Toledo <agc.rodrigo@gmail.com>
2012-06-14 01:30:34 -0300

    Alterações na home Menu superior com Perto de você

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,8 +9,8 @@ module ApplicationHelper
 			['Tá rolando', root_path],
 			['Vai bombar', root_path],
 			['Galeria', root_path],
+			['Perto de você', root_path],
 			['Promoções', root_path],
-			['Que site é esse?', root_path],
 			['Fala aí!', root_path],
 		]
 		content_tag(:ul, links.collect{|t| content_tag(:li, link_to(t[0],t[1]))}.join.html_safe, :id => 'menu').html_safe
