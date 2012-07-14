9b3450afb31f39ce2ce0f2a993f4e03601b8f796
Rodrigo Toledo <agc.rodrigo@gmail.com>
2012-07-14 13:51:50 -0300

    precompile

--- config/application.rb
+++ config/application.rb
@@ -52,6 +52,7 @@ module OnTheParty
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.precompile += ['frontend/html5.js']
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
