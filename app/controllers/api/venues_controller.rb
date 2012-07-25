154cb0d2160bd0b744d7f912f681473e3b0fb340
Dener Wilian Pereira do Carmo <dener.wilian@gmail.com>
2012-07-25 00:26:12 -0400

    Ajustado filtro

--- app/controllers/api/venues_controller.rb
+++ app/controllers/api/venues_controller.rb
@@ -1,6 +1,9 @@
 class Api::VenuesController < ApplicationController
+  skip_before_filter :verify_authenticity_token, only: [:find_or_create]
+
+
   def find_or_create
-    venue = Venue.find_or_create(params[:venue])
+    venue = Venue.find_or_create(params[:foursquare_params])
 
     
     respond_to do |format|
