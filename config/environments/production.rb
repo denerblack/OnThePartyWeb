5343229a2b3c6dd145730e1f4fb87fc35d6cb21c
DEner <ontheparty.com.br@li467-32.members.linode.com>
2012-09-03 15:10:46 +0000

    enviando configuracoes de producao

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ OnTheParty::Application.configure do
   # config.assets.manifest = YOUR_PATH
 
   # Specifies the header that your server uses for sending files
-  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
+  config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
@@ -48,7 +48,7 @@ OnTheParty::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  # config.assets.precompile += %w( frontend/home.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
